59d30841de17f69ad2c33bd3737f8d33183f43a9
RR-Vignesh <vignesh31794@gmail.com>
2022-12-05 22:34:21 -0500

    Adding the updated scripts to publish the message to chatter topic

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,16 +1,24 @@
-// Copyright 2022 Vignesh Ravichandran Radhakrishnan (rr94@umd.edu)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* MIT License
+
+Copyright (c) 2022 Vignesh Ravichandran Radhakrishnan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
 
 #include <chrono>
 #include <functional>
@@ -45,7 +53,7 @@ class MinimalPublisher : public rclcpp::Node {
 public:
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
     try {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
       auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
       param_desc.description = "This parameter is updated by given argument in "
                                "launch file and it is "
